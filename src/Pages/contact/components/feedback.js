7cec1960c6126ae51f7f01d8537bde423a9ae77c
Hamo Muradyan <hamlet@advancedtech.am>
2022-12-27 17:42:26 +0400

    lowercase e for phone

--- src/Pages/contact/components/feedback.js
+++ src/Pages/contact/components/feedback.js
@@ -6,7 +6,7 @@ function BasicExample() {
       <h5>
         You can contact us.
       </h5>
-      <h5>by Email: info@paladiumtechserviceltd.com</h5>
+      <h5>by email: info@paladiumtechserviceltd.com</h5>
       <h5>by phone: +971 52 725 68 19</h5>
     </div>
   );
