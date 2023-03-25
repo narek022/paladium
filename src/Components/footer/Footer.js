b2a6951eada8378e106b95366da319f6035b9392
Narek211 <nareksaruxanyan95@gmail.com>
2023-03-26 01:59:42 +0400

    mobile menu

--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { useEffect } from "react";
 import "./style.css";
 
 function Footer () {
-
 return(
   <footer className="page-footer font-small blue pt-4">
     <div className="container-fluid">
