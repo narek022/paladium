50f7d0c5f1689ce680421e2d802b2da30629a8a3
Hamo Muradyan <hamlet@advancedtech.am>
2023-05-04 18:19:39 +0400

    add google analytics v1

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import ReactGA from "react-ga4";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+ReactGA.initialize("G-R6N9LGJQEV");
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -14,4 +16,11 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
+
+reportWebVitals(SendAnalytics);
